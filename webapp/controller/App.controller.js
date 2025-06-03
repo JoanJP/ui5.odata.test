c3f650cd1721ade16ebebb8a9199f4bfafd2431f
JoanJP <panggestu3214@gmail.com>
2025-06-03 15:28:58 +0700

    add edit, save edit, soft delete selected rows

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,44 +6,90 @@ sap.ui.define(
     "sap/ui/model/FilterOperator",
     "sap/ui/core/Fragment",
     "sap/ui/Device",
-    "sap/m/ColumnListItem",
   ],
-  (
-    BaseController,
-    Filter,
-    Sorter,
-    FilterOperator,
-    Fragment,
-    Device,
-    ColumnListItem
-  ) => {
+  (BaseController, Filter, Sorter, FilterOperator, Fragment, Device) => {
     "use strict";
 
     return BaseController.extend("ui5.odata.test.controller.App", {
       onInit() {
-        //* Keeps reference to any of the created sap.m.ViewSettingsDialog-s in this sample
-        this._mViewSettingsDialogs = {};
+        //* Dialog object
+        this._mDialogs = {};
+      },
+      //! Dialog Function
+      async _getDialog(sFragmentName, sDialogId) {
+        if (!this._mDialogs[sFragmentName]) {
+          const oDialog = await Fragment.load({
+            id: sDialogId || this.getView().getId(),
+            name: sFragmentName,
+            controller: this,
+          });
+          this.getView().addDependent(oDialog);
+          this._mDialogs[sFragmentName] = oDialog;
+        }
+        return this._mDialogs[sFragmentName];
+      },
+      async _onDialogCancelPress(sDialogName) {
+        const sFragmentId = this.getView().getId();
+        const oDialog = Fragment.byId(sFragmentId, sDialogName);
+        if (oDialog) {
+          oDialog.close();
+        }
+      },
 
-        //* Open Column Menu
-        var oView = this.getView();
-        Fragment.load({
-          id: this.getView().getId(),
-          name: "ui5.odata.test.view.fragment.ColumnMenu",
-          controller: this,
-        }).then(function (oMenu) {
-          oView.addDependent(oMenu);
-          return oMenu;
-        });
+      //! Get the First Selected Row
+      async _getFirstRow({ tableId = "appTable", returnObject = true } = {}) {
+        const oTable = this.byId(tableId);
+        const aSelectedContexts = oTable.getSelectedContexts();
+        if (aSelectedContexts.length === 0) {
+          sap.m.MessageToast.show("No row selected.");
+          return null;
+        }
+        return returnObject
+          ? aSelectedContexts[0].getObject()
+          : aSelectedContexts[0];
+      },
+
+      //! Get the Selected Rows
+      async _getSelectedRows({
+        tableId = "appTable",
+        returnObject = true,
+      } = {}) {
+        const oTable = this.byId(tableId);
+        const aSelectedContexts = oTable.getSelectedContexts();
+        if (aSelectedContexts.length === 0) {
+          sap.m.MessageToast.show("No row selected.");
+          return null;
+        }
+        return returnObject ? aSelectedContexts.getObject() : aSelectedContexts;
+      },
+
+      //! Delete (Soft Delete)
+      async onDeletePress() {
+        const oData = await this._getSelectedRows({ returnObject: false });
+        if (!oData) {
+          // Check if no row is selected
+          return; // Exit early
+        }
+        try {
+          oData.forEach(function (oData) {
+            oData.setProperty("isDeleted", true);
+          });
+          sap.m.MessageToast.show("Delete success");
+          const oTable = this.byId("appTable");
+          const oBinding = oTable.getBinding("items");
+          oBinding.refresh();
+        } catch (error) {
+          console.error("Error while Deleting", error);
+        }
       },
 
       //! Create
       async onCreatePress(oEvent) {
         // create dialog lazily
-        this.oDialog ??= await this.loadFragment({
-          name: "ui5.odata.test.view.fragment.CreateData",
-        });
-
-        this.oDialog.open();
+        const oDialog = await this._getDialog(
+          "ui5.odata.test.view.fragment.CreateData"
+        );
+        oDialog.open();
       },
       async onCreateConfirmPress(oEvent) {
         var oTitle = this.byId("inputTitle").getValue();
@@ -67,17 +113,71 @@ sap.ui.define(
           var oTable = this.byId("appTable");
           var oBinding = oTable.getBinding("items");
           oBinding.refresh();
+          // Reset input fields when opening the dialog
+          this.byId("inputTitle").setValue("");
+          this.byId("inputIsbn").setValue("");
+          this.byId("inputDesc").setValue("");
+          this.byId("inputStock").setValue("");
+          this.byId("inputPrice").setValue("");
           this.byId("createDialog").close();
         } catch (error) {
-          console.error("Error", error);
+          console.error("Error when creating", error);
         }
       },
       onCreateCancelPress: async function () {
-        this.byId("createDialog").close();
+        // Reset input fields when opening the dialog
+        this.byId("inputTitle").setValue("");
+        this.byId("inputIsbn").setValue("");
+        this.byId("inputDesc").setValue("");
+        this.byId("inputStock").setValue("");
+        this.byId("inputPrice").setValue("");
+        this._onDialogCancelPress("createDialog");
       },
 
       //! Edit
-      onEditPress() {},
+      async onEditPress() {
+        const oData = await this._getFirstRow();
+        if (!oData) {
+          // Check if no row is selected
+          return; // Exit early
+        }
+        const fragmentId = this.getView().getId();
+        const oEditDialog = await this._getDialog(
+          "ui5.odata.test.view.fragment.EditData"
+        );
+        // Set the context to the dialog so fields are pre-filled
+        Fragment.byId(fragmentId, "editInputTitle").setValue(oData.title);
+        Fragment.byId(fragmentId, "editInputIsbn").setValue(oData.isbn);
+        Fragment.byId(fragmentId, "editInputDesc").setValue(oData.descr);
+        Fragment.byId(fragmentId, "editInputStock").setValue(oData.stock);
+        Fragment.byId(fragmentId, "editInputPrice").setValue(oData.price);
+        oEditDialog.setModel(this.getView().getModel());
+        oEditDialog.open();
+      },
+      async onEditConfirmPress() {
+        const oData = await this._getFirstRow({ returnObject: false });
+        const oTitle = this.byId("editInputTitle").getValue();
+        const oIsbn = this.byId("editInputIsbn").getValue();
+        const oDesc = this.byId("editInputDesc").getValue();
+        const oStock = parseInt(this.byId("editInputStock").getValue());
+        const oPrice = this.byId("editInputPrice").getValue();
+
+        try {
+          // Set new values on the context
+          oData.setProperty("title", oTitle);
+          oData.setProperty("isbn", oIsbn);
+          oData.setProperty("descr", oDesc);
+          oData.setProperty("stock", oStock);
+          oData.setProperty("price", oPrice);
+          sap.m.MessageToast.show("Book updated successfully");
+          this.byId("editDialog").close();
+        } catch (error) {
+          console.error("Error when updating", error);
+        }
+      },
+      onEditCancelPress: function () {
+        this._onDialogCancelPress("editDialog");
+      },
 
       //! Filter
       onFilterItems(oEvent) {
@@ -95,11 +195,10 @@ sap.ui.define(
 
       //! Sort
       async onSortPress() {
-        this.getViewSettingsDialog(
+        const oDialog = await this._getDialog(
           "ui5.odata.test.view.fragment.SortDialog"
-        ).then(function (oViewSortDialog) {
-          oViewSortDialog.open();
-        });
+        );
+        oDialog.open();
       },
       onSortDialogConfirm: function (oEvent) {
         var oTable = this.byId("appTable"),
@@ -113,40 +212,6 @@ sap.ui.define(
         aSorters.push(new Sorter(sPath, bDescending));
         // apply the selected sort and group settings
         oBinding.sort(aSorters);
-      },
-      getViewSettingsDialog: function (sDialogFragmentName) {
-        var pDialog = this._mViewSettingsDialogs[sDialogFragmentName];
-        if (!pDialog) {
-          pDialog = Fragment.load({
-            id: this.getView().getId(),
-            name: sDialogFragmentName,
-            controller: this,
-          }).then(function (oDialog) {
-            if (Device.system.desktop) {
-              oDialog.addStyleClass("sapUiSizeCompact");
-            }
-            return oDialog;
-          });
-          this._mViewSettingsDialogs[sDialogFragmentName] = pDialog;
-        }
-        return pDialog;
-      },
-
-      //! Sort in Column
-      onSortChange(oEvent) {
-        const oTable = this.byId("appTable");
-        const oBinding = oTable.getBinding("items");
-        const oQuickSortItem = oEvent.getParameter("item");
-        if (oQuickSortItem.getSortOrder() === "None") {
-          oBinding.sort();
-        } else {
-          oBinding.sort([
-            new Sorter(
-              oQuickSortItem.getKey(),
-              oQuickSortItem.getSortOrder() === "Descending"
-            ),
-          ]);
-        }
       },
     });
   }
